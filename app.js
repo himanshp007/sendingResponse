54d74ee2e771754e9b647c8420f04c3ce73965f7
himanshp007 <himanshpatel192@gmail.com>
2024-03-22 05:48:24 +0530

    created a server and sent a html response which puts different text on different routes

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => console.log("Himansh"));
+const server = http.createServer((req, res) =>{
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node App</title></head>');
+
+    if (req.url === '/home'){
+        res.write('<body><h1>Welcome home</h1></body>');
+    } else if (req.url === '/about') {
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+    }else if (req.url === '/node'){
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    }
+    
+    res.write('</html>');
+});
 
 server.listen(4000);
